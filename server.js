e0f6ce8c93ee7221dae8e61b233648ce93aa1524
Kalia Thao <kaliathao21@gmail.com>
2023-11-24 12:23:18 -0800

    Added GET notes route and *

--- server.js
+++ server.js
@@ -17,8 +17,13 @@ app.use('/api', api);
 
 app.use(express.static('public'));
 
-// GET Route for homepage
-app.get('/', (req, res) =>
+// GET Route for notes
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// GET Route for index.html
+app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
